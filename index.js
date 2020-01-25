7c5090246de9ff9aa2ca4f32cd8ee706235bb8f2
Merlijn03 <54846482+Merlijn03@users.noreply.github.com>
2020-01-25 14:11:15 +0100

    opdracht 2

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 var gerechten = ["Macaroni","Lasagna","Soep van de dag","Salade","Friet","Biefstuk","Pizza","Sushi"];
-var uitvoer = document.getElementById('uitvoer');
-var menu = [];
+var body = document.getElementById('body');
 
-function showArray() {
+function showArrayOrdered() {
   for (var i = 0; i < gerechten.length; i++) {
-    console.log(gerechten[i]);
+    var ol = document.getElementById('ol');
+    var newLi = document.createElement("li");
+    var newContent = document.createTextNode(gerechten[i]);
+    ol.appendChild(newLi);
+    newLi.appendChild(newContent);
   }
 }
 
-function printElement(number) {
-  uitvoer.innerHTML = gerechten[number];
+function showArrayUnordered() {
+  for (var i = 0; i < gerechten.length; i++) {
+    var ul = document.getElementById('ul');
+    var newLi = document.createElement("li");
+    var newContent = document.createTextNode(gerechten[i]);
+    ul.appendChild(newLi);
+    newLi.appendChild(newContent);
+  }
 }
 
-function returnElement() {
-  return gerechten[5];
+function showArrayList(list) {
+  var listType = document.createElement(list);
+  body.appendChild(listType);
+  for (var i = 0; i < gerechten.length; i++) {
+    var newLi = document.createElement("li");
+    var newContent = document.createTextNode(gerechten[i]);
+    listType.appendChild(newLi);
+    newLi.appendChild(newContent);
+  }
 }
 
-showArray();
-printElement(3);
-menu.push(returnElement());
+showArrayOrdered();
+showArrayUnordered();
+showArrayList('ol');
